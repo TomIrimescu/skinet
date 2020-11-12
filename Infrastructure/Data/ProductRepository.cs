88684a760d249050996d29b4d56e8ed08955969b
Tom Irimescu <tom@irimescu.com>
2020-11-12 16:08:23 -0500

    Add repo methods for di into products controller

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -2,20 +2,27 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class ProductRepository : IProductRepository
+  public class ProductRepository : IProductRepository
+  {
+    private readonly StoreContext _context;
+    public ProductRepository(StoreContext context)
     {
-      public Task<Product> GetProductByIdAsync(int id)
-      {
-        throw new System.NotImplementedException();
-      }
-
-      public Task<IReadOnlyList<Product>> GetProductsAsync()
-      {
-        throw new System.NotImplementedException();
-      }
-        
+      _context = context;
     }
+
+    public async Task<Product> GetProductByIdAsync(int id)
+    {
+      return await _context.Products.FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<Product>> GetProductsAsync()
+    {
+      return await _context.Products.ToListAsync();
+    }
+
+  }
 }
